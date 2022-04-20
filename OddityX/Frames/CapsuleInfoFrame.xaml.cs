fa119ebad6bc47f9acef2abba7c7afc999114115
Konstantin Sharykin <kotyshe@gmail.com>
2022-04-20 15:55:20 +0200

    Update icons. Add crew and launches

--- OddityX/Frames/CapsuleInfoFrame.xaml.cs
+++ OddityX/Frames/CapsuleInfoFrame.xaml.cs
@@ -5,7 +5,10 @@ using Microsoft.UI.Xaml.Data;
 using Microsoft.UI.Xaml.Input;
 using Microsoft.UI.Xaml.Media;
 using Microsoft.UI.Xaml.Navigation;
+using Oddity;
 using Oddity.Models.Capsules;
+using Oddity.Models.Crew;
+using Oddity.Models.Launches;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -25,20 +28,79 @@ namespace OddityX.Frames
     public sealed partial class CapsuleInfoFrame : Page
     {
         CapsuleInfo currentCupsule;
+        OddityCore oddity; 
+        
         public CapsuleInfoFrame()
         {
             this.InitializeComponent();
+            oddity = new OddityCore();
         }
 
         protected override void OnNavigatedTo(NavigationEventArgs e)
         {
             currentCupsule = e.Parameter as CapsuleInfo;
-            Serial.Text = $"Serial: {currentCupsule.Serial}";
-            Status.Text = $"Status: {currentCupsule.Status.ToString()}";
-            ReuseCount.Text = $"Reused count: {currentCupsule.ReuseCount}";
+            Serial.Text = $"Serial: {currentCupsule?.Serial}";
+            Status.Text = $"Status: {currentCupsule?.Status}";
+            ReuseCount.Text = $"Reused count: {currentCupsule?.ReuseCount}";
             WaterLandings.Text = $"Water landings: {currentCupsule?.WaterLandings?.ToString()}";
             LandLandings.Text = $"Land landings: {currentCupsule?.LandLandings?.ToString()}";
-            CountLaunches.Text = $"Count launches: {currentCupsule.Launches.Count}";
+            LastUpdate.Text = $"$Last update: {currentCupsule?.LastUpdate}";
+            CountLaunches.Text = $"Count launches: {currentCupsule?.Launches.Count}";
+        }
+
+        private async void ExpandLaunchesNameExpanding(Expander sender, ExpanderExpandingEventArgs args)
+        {
+            /*var value = currentCupsule.Launches[0].Value;
+            LaunchesList.ItemsSource = value;*/
+            
+            var launchListId = currentCupsule.LaunchesId;
+            var launches = await oddity.LaunchesEndpoint.GetAll().ExecuteAsync();
+            List<LaunchInfo> capsuleLaunches = new();
+            launchListId.ForEach(launchId => capsuleLaunches.AddRange(launches.Where(l => l.Id == launchId)));
+
+            if (!capsuleLaunches.Any())
+            {
+                var newInfo = new LaunchInfo() { Name = "Doesn't have launches" };
+                capsuleLaunches.Add(newInfo);
+            }
+
+            LaunchesList.ItemsSource = capsuleLaunches;
+
+            LoadListProgress.Visibility = Visibility.Collapsed;
+            LaunchesList.Visibility = Visibility.Visible;
+        }
+
+        private async void ExpandCrewExpanding(Expander sender, ExpanderExpandingEventArgs args)
+        {
+            var launchListId = currentCupsule.LaunchesId;
+            var launches = await oddity.LaunchesEndpoint.GetAll().ExecuteAsync();
+            var crews = await oddity.CrewEndpoint.GetAll().ExecuteAsync();
+
+            List<LaunchInfo> capsuleLaunches = new();
+            List<CrewInfo> capsuleCrew = new(); 
+            launchListId.ForEach(launchId => capsuleLaunches.AddRange(launches.Where(l => l.Id == launchId)));
+
+            if (!capsuleLaunches.Any())
+            {
+                capsuleCrew.Add(new CrewInfo() { Name = "Haven't had a crew yet" });
+            }
+            else
+            {
+                foreach (var crewId in capsuleLaunches.SelectMany(launch => launch.CrewId))
+                {
+                    capsuleCrew.AddRange(crews.Where(crew => crew.Id == crewId));
+                }
+            }
+
+            if (!capsuleCrew.Any())
+            {
+                capsuleCrew.Add(new CrewInfo() {Name = "Haven't had a crew yet"});
+            }
+
+            CrewList.ItemsSource = capsuleCrew;
+
+            LoadCrewProgress.Visibility = Visibility.Collapsed;
+            CrewList.Visibility = Visibility.Visible;
         }
     }
 }
