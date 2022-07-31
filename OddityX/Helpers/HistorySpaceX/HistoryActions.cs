2bdbc6b0ba0c1cf328ee17de7d144c011ac8f789
hunterlan <kotyshe@gmail.com>
2022-07-31 13:20:09 +0200

    Add ability get history events

---
+++ OddityX/Helpers/HistorySpaceX/HistoryActions.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using Newtonsoft.Json;
+
+namespace OddityX.Helpers.HistorySpaceX;
+
+public class HistoryActions
+{
+    private const string HistoryUrl = "https://api.spacexdata.com/v4/history";
+
+    private readonly WebClient _client;
+
+    [Obsolete("Obsolete")]
+    public HistoryActions()
+    {
+        _client = new WebClient();
+    }
+
+    public HistoryModel GetOne(string historyId)
+    {
+        string json = _client.DownloadString(HistoryUrl + $"/{historyId}");
+
+        var history = JsonConvert.DeserializeObject<HistoryModel>(json);
+
+        return history;
+    }
+
+    public List<HistoryModel> GetAll()
+    {
+        string json = _client.DownloadString(HistoryUrl);
+
+        var history = JsonConvert.DeserializeObject<List<HistoryModel>>(HistoryUrl);
+
+        return history;
+    }
+
+    ~HistoryActions()
+    {
+        _client.Dispose();
+    }
+}
---
+++ OddityX/Helpers/HistorySpaceX/HistoryModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OddityX.Helpers.HistorySpaceX
+{
+    public class HistoryModel
+    {
+        public string Id { get; set; }
+        
+        public string Title { get; set; }
+
+        public DateTime DateUtc { get; set; }
+
+        public long DateUnix { get; set; }
+
+        public string Details { get; set; }
+
+        private List<string> Links { get; set; }
+    }
+}
